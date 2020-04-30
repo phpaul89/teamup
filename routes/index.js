991c9c6536c501712558bd185577c4238beaedb2
phpaul89 <53011989+phpaul89@users.noreply.github.com>
2020-04-30 20:35:43 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const router = express.Router();
 
 /* GET home page */
-router.get("/", (request, response, next) => {
+router.get("../", (request, response, next) => {
   response.render("index");
 });
 
